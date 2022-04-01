cbcfbdb995f61d3228c22379dc77ca33be46f69e
Rachel Bock <rachel.online@gmail.com>
2022-04-01 12:55:35 -0600

    Add medusa class functionality

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name){
+        this.name = name;
+        this.statues = [];
+    }
+
+    gazeAtVictim(victimObj) {
+        if(this.statues.length < 3){
+            var newStatue = new Statue(victimObj.name);
+            this.statues.push(newStatue);    
+        } else {
+            var newPerson = new Person(this.statues.shift().name);
+            newPerson.mood = 'relieved';
+            var newVictim = new Statue(victimObj.name);
+            this.statues.push(newVictim);
+            return newPerson;
+        }
+    }
+}
+
+module.exports = Medusa;
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,12 @@
+const Statue = require("./statue");
+
+class Person {
+    constructor(name){
+        this.name = name;
+        this.mood = 'frightened'
+    }
+
+    
+}
+
+module.exports = Person;
