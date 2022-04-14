32d9c76ae1793e2f080f2b9d1c7c299fdfe12afd
Rachel Bock <rachel.online@gmail.com>
2022-04-14 08:24:53 -0600

    Add Being class to Spaceport

--- spaceport/src/being.js
+++ spaceport/src/being.js
@@ -1,4 +1,14 @@
 class Being {
+  constructor(name, species) {
+    this.name = name;
+    this.credits = 0;
+    this.isAlive = true;
+    this.species = species;
+  }
+
+  updateCredits(amount) {
+    this.credits += amount;
+  }
 }
 
 module.exports = Being;
