eac9da4054a304240ca7ff706350036f4793393b
Rachel Bock <rachel.online@gmail.com>
2022-05-05 08:20:45 -0600

    Add beginning of lunchbox snack class functionality

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,24 @@
 class Snack {
+  constructor(snackName) {
+    this.amount = 100;
+    this.cuttingItClose = false;
+    this.deliciousLevel = 'extra';
+    this.isInLunchBox = false;
+    this.type = snackName;
+ }
 
+ checkForHealthy() {
+  //  if (this.type.contains('ruit')) {
+
+  //  }
+ }
+
+ getEaten() {
+   this.amount = this.amount - 10;
+   if (this.amount <= 20) {
+     this.cuttingItClose = true;
+   }
+ }
 }
 
 module.exports = Snack;
